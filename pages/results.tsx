81a85c2970ca38fd2c2ce5faa72fe66a9ddde78a
Chris <chirssel@gmail.com>
2022-04-30 16:25:53 +0200

    Change page title to search query on the results page

--- pages/results.tsx
+++ pages/results.tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps, NextPage } from 'next';
+import Head from 'next/head';
 import tw from 'twin.macro';
 import SearchResult from '~/components/search/SearchResult';
 import { getSearchResults, SearchResults } from '~/lib/search';
@@ -7,9 +8,13 @@ import { formatNumber, removeUndefined } from '~/lib/util';
 const Header = tw.p`py-2 border-b border-gray-200 text-gray-700 font-semibold`;
 const Container = tw.div`container max-w-5xl mx-auto px-4 py-4 flex flex-col gap-4`;
 
-const Results: NextPage<{ results: SearchResults }> = ({ results }) => {
+const Results: NextPage<{ results: SearchResults; query: string }> = ({ results, query }) => {
 	return (
 		<Container>
+			<Head>
+				<title>{query}</title>
+			</Head>
+
 			{results.results && <Header>{formatNumber(results.results)} results</Header>}
 			{results.items.map((result, i) => (
 				<SearchResult key={i} result={result} />
@@ -30,6 +35,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 	return {
 		props: removeUndefined({
 			results: await getSearchResults(context.query.search_query as string),
+			query: context.query.search_query as string,
 		}),
 	};
 };
