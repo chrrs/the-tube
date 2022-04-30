cbfbfe07864706a75a91da2305708af916d6c3ae
Chris <chirssel@gmail.com>
2022-04-30 16:44:41 +0200

    Actually allow searching from the search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,16 +1,25 @@
+import { FormEvent, useState } from 'react';
 import tw from 'twin.macro';
-
 import { RiSearchLine } from 'react-icons/ri';
+import { useRouter } from 'next/router';
 
-const Wrapper = tw.div`flex w-[48rem]`;
+const Wrapper = tw.form`flex w-[48rem]`;
 const Input = tw.input`flex-1 border border-gray-300 focus-within:border-blue-300 px-2 py-1`;
 const Button = tw.button`border border-l-0 border-gray-300 bg-gray-100 text-gray-700 hover:(bg-gray-200 text-black) px-6 py-1`;
 
 const SearchBar: React.FC = () => {
+	const router = useRouter();
+	const [query, setQuery] = useState('');
+
+	function submitSearch(e: FormEvent) {
+		e.preventDefault();
+		router.push(`/results?search_query=${query}`);
+	}
+
 	return (
-		<Wrapper>
-			<Input />
-			<Button>
+		<Wrapper onSubmit={submitSearch}>
+			<Input onChange={(e) => setQuery(e.target.value)} />
+			<Button type="submit">
 				<RiSearchLine />
 			</Button>
 		</Wrapper>
