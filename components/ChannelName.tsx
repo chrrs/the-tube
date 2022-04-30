4d883388cd62e17278f35639438fd6733bde3184
Chris <chirssel@gmail.com>
2022-04-30 16:45:21 +0200

    Improve verified badge alignment

--- components/ChannelName.tsx
+++ components/ChannelName.tsx
@@ -1,7 +1,7 @@
 import tw, { styled } from 'twin.macro';
 import { RiCheckLine } from 'react-icons/ri';
 
-const VerifiedBadge = styled(RiCheckLine, tw`inline-block align-middle ml-1 mb-0.5`);
+const VerifiedBadge = styled(RiCheckLine, tw`inline-block align-middle ml-1 mb-[0.125em]`);
 
 const ChannelName: React.FC<{
 	verified?: boolean;
