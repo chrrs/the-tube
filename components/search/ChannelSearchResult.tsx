cfa4dbfd8dafb578062af2189034357299bc4d8b
Chris <chirssel@gmail.com>
2022-05-02 21:32:16 +0200

    Add gray background to profile pictures

--- components/search/ChannelSearchResult.tsx
+++ components/search/ChannelSearchResult.tsx
@@ -6,7 +6,7 @@ import ChannelName from '../ChannelName';
 const Wrapper = tw.div`flex gap-4`;
 const AvatarWrapper = styled.div({ ...tw`flex justify-center`, flex: '0 0 360px' });
 const Info = tw.div`flex flex-col justify-center`;
-const Avatar = tw.img`w-32 h-32 rounded-full overflow-hidden`;
+const Avatar = tw.img`w-32 h-32 rounded-full overflow-hidden bg-gray-200`;
 const Name = tw.h1`text-lg font-medium`;
 const Subtitle = tw.p`text-gray-700`;
 const Description = tw.p`text-gray-700 mt-2 line-clamp-1`;
